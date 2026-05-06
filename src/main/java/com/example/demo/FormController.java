5131e9182f5915ddda37144303ca7479458c14c5
Edgar Griffin <edgarcgriffinjr@gmail.com>
2026-05-06 19:23:43 -0400

    initial commit

---
+++ src/main/java/com/example/demo/FormController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class FormController {
+
+    @GetMapping("/")
+    public String showForm(Model model) {
+        model.addAttribute("user", new User());
+        return "form";
+    }
+
+    @PostMapping("/submit")
+    public String submitForm(@ModelAttribute User user, Model model) {
+        model.addAttribute("user", user);
+        return "result";
+    }
+}
